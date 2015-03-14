4b5ecee86b8e05acac44bdd41e021d55300a1d30
Hanne Jalborg <hanne.jalborg@gmail.com>
2015-03-14 20:13:57 +0100

    New Javascript file

--- test.js
+++ test.js
@@ -1,30 +1,9 @@
-function myFunction() {
-    var userKommun = document.getElementById("myText").value;
-
-var xmlhttp = new XMLHttpRequest();
-var url = "kommuner.json";
-
-var xhr = new XMLHttpRequest 
-xhr.open("GET", url, false );
-xhr.send();
-
-//Change from text to JSON format
-var jsonData = JSON.parse(xhr.responseText);
-console.log(jsonData);
-
-var komId= jsonData.kommun;
-
-for(var i = 0; i < komId.length; i++) {
-  if(komId[i].Kommuner == userKommun){
-    var newKommunId = komId[i].KommunId;
-    console.log(newKommunId);
-
 // Api reguest to get all ads from an area
 var kommunId = "1080"; // Depends on user input
 var nyckelOrd = "sommarjobb"; //This is always decided of us
 var rad = "30"; //This is always decided (we can also have more pages)
 
-var url = "http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?kommunid=" + newKommunId + "&nyckelord=" + nyckelOrd + "&antalrader=" + rad;
+var url = "http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?kommunid=" + kommunId + "&nyckelord=" + nyckelOrd + "&antalrader=" + rad;
 console.log(url);
 
 //API call
@@ -44,15 +23,30 @@ for(var i = 0; i < result.length; i++) {
 	var yrkesBenamning = result[i].yrkesbenamning;
 	var annonsId = result[i].annonsid;
 
+
+
 document.getElementById("annonser").innerHTML += "<div class='ad'>" + annonsRubrik + "<br>" + arbetsplatsNamn + "<br>" + yrkesBenamning + "<br>" + "<input type='image' src='plus_icon.png' id=annonsimage>" + "<div id='annonsid' style='display:none'>" + annonsId + "</div>" + "</div>" + "<br>";
 //function loadAd(adId){
 //	console.log(adId);
 }// This function is not working as I want to, want to get the right annonsId value when I click the ugly plus symbol :)
 
 
+//Hanne newcode
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    return this;
+}
+
 $("input#annonsimage").click(function() {
 	var annonsid = $(this).next().html();
 	$(".helannons").show();
+	$(".helannons").center();
+	console.log(annonsid);
+	
+
+
 
 //To get more detailed ads with input from the list
 //var id = "6197460"; //This will be the annonsId output from the list
@@ -73,7 +67,8 @@ document.getElementById("annonsrubrik").innerHTML = annonsData.platsannons.annon
 document.getElementById("yrke").innerHTML = annonsData.platsannons.annons.yrkesbenamning;
 document.getElementById("annonstext").innerHTML = annonsData.platsannons.annons.annonstext;
 document.getElementById("foretag").innerHTML = annonsData.platsannons.arbetsplats.arbetsplatsnamn;
-document.getElementById("sok-knapp").innerHTML = annonsData.platsannons.annons.platsannonsUrl;
+//Hanne newcode
+$("#sok-knapp-button").attr('href', annonsData.platsannons.annons.platsannonsUrl);
 document.getElementById("sista-dag").innerHTML = annonsData.platsannons.ansokan.sista_ansokningsdag;
 
 //Address output for the travel calculation
@@ -84,7 +79,18 @@ var postort = annonsData.platsannons.arbetsplats.postort;
 console.log(postadress);
 console.log(postnummer);
 console.log(postort);
+
+/*
+$(".helannons").hide();
+$("input#annonsimage").click(function() {
+	var annonsid = $(this).next().html();
+	$(".helannons").slideToggle();
+    });
+
+
+*/
+//Hanne newcode - change to button so that the entire ad doesn't go away
+//$(".helannons").click(function() {
+//$(".helannons").hide();
+//	})
 })
-}
-}
-}
