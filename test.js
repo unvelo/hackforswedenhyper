44856af543103b607998ff7972f6de0d0caa3e12
Jessicaalbers <jessica.albers@hyperialand.se>
2015-03-18 11:04:37 +0100

    GA + prototyp text

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
+$("#skattwrap").hide();
+  $("#sok-knapp").click(function() {
+            $("#skattwrap").show();
+  });
 function myFunction() {
 
+ //GET KOMMUN ID
   var userInput = document.getElementById("myText").value;
   var userKommun = userInput.toLowerCase();
+  
   var xmlhttp = new XMLHttpRequest();
   var url1 = "kommuner_lowercase.json";
-
   var xhr = new XMLHttpRequest 
   xhr.open("GET", url1, false );
   xhr.send();
 
-  //Change from text to JSON format
   var jsonData = JSON.parse(xhr.responseText);
   console.log(jsonData);
-
   var komId= jsonData.kommun;
 
   for (var i = 0; i < komId.length; i++) {
@@ -21,56 +24,37 @@ function myFunction() {
       var newKommunId = komId[i].KommunId;
       console.log(newKommunId);
 
+    //GET # OF PEOPLE WHO IS PAYING TAX
+
       var xml = new XMLHttpRequest();
       var url2 = "taxpayers_lowercase.json";
-
       var xhr = new XMLHttpRequest 
       xhr.open("GET", url2, false );
       xhr.send();
-
-      //Change from text to JSON format
+    
       var taxData = JSON.parse(xhr.responseText);
       console.log(taxData);
-
       var taxPeople = taxData.kommun;
 
       for (var i = 0; i < taxPeople.length; i++) {
-
         if (taxPeople[i].kommune == userKommun) {
           var taxPayers = taxPeople[i].taxpayers;
           document.getElementById("skatt").innerHTML = taxPayers;
 
-          // Api reguest to get all ads from an area
-          var nyckelOrd = "'butikssäljare'OR'barnvakt'OR'butikssäljare'OR'barnflicka'OR'extrainhopp'OR'varuplockare'OR'lagerarbetare'OR'cafévärd'OR'Städare'OR'tidningsdistributör'OR'enklare städuppgifter'OR'kökshjälp'OR'burger king'OR'butiksmedarbetare'OR'cafépersonal'OR'campingvärd'OR'campingvärder'OR'sommarpersonal'OR'servitris'OR'servitör'OR'butikssäljare'OR'mcdonalds'OR'sommarjobb'OR'tidningsbud'OR'brevbärare'OR'tidningsdistributörer'OR'tidningsdistributör'OR'Cafebiträde'OR'diskare'OR'sommarmedarbetare'OR'Köksbiträde'OR'serveringspersonal'OR'reklamutdelning'OR'kassapersonal'OR'minst 16 år'OR'minst 17 år'OR'minst 18 år'AND'deltid"; //This is always decided of us
-
-          var rad = "30"; //This is always decided (we can also have more pages)
-
+          //GET ALL THE ADS
+          var nyckelOrd = "'butikssäljare'OR'barnvakt'OR'butikssäljare'OR'barnflicka'OR'extrainhopp'OR'varuplockare'OR'lagerarbetare'OR'cafévärd'OR'Städare'OR'tidningsdistributör'OR'enklare städuppgifter'OR'kökshjälp'OR'burger king'OR'butiksmedarbetare'OR'cafépersonal'OR'campingvärd'OR'campingvärder'OR'sommarpersonal'OR'servitris'OR'servitör'OR'butikssäljare'OR'mcdonalds'OR'sommarjobb'OR'tidningsbud'OR'brevbärare'OR'tidningsdistributörer'OR'tidningsdistributör'OR'Cafebiträde'OR'diskare'OR'sommarmedarbetare'OR'Köksbiträde'OR'serveringspersonal'OR'reklamutdelning'OR'kassapersonal'OR'minst 16 år'OR'minst 17 år'OR'minst 18 år'AND'deltid";
+          var rad = "30";
           var url = "http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?kommunid=" + newKommunId + "&nyckelord=" + nyckelOrd + "&antalrader=" + rad;
           console.log(url);
 
-          /*$(function() {
-            $('a.page-scroll').bind('click', function(event) {
-              var $anchor = $(this);
-                $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-                }, 
-                5000, 'easeInOutExpo');
-                event.preventDefault();
-              });
-          });*/
-
-          //API call
           var xhr = new XMLHttpRequest 
           xhr.open("GET", url, false );
           xhr.send();
 
-          //Change from text to JSON format
           var jsonData = JSON.parse(xhr.responseText);
           console.log(jsonData);
           var result = jsonData.matchningslista.matchningdata;
 
-
-          //Get the wanted information from the API
           for(var i = 0; i < result.length; i++) {
             var annonsRubrik = result[i].annonsrubrik;
             var arbetsplatsNamn = result[i].arbetsplatsnamn;
@@ -78,6 +62,9 @@ function myFunction() {
             var annonsId = result[i].annonsid;
 
             console.log(annonsId);
+
+            document.getElementById("annonser").innerHTML += "<div class='ad'> <div class='wrap-ad'>" +  "<div class='headline'>" + annonsRubrik + "</div>" + "<br>" + "<div class='undertitle'>" + arbetsplatsNamn + "<br>" + yrkesBenamning + "</div>" + "</div>" + "<br>" + "<input type='image' src='plus.png' id=annonsimage>" + "<div id='annonsid' style='display:none'>" + annonsId + "</div>" + "</div>";
+
             //Hanne newcode
             jQuery.fn.center = function () {
                 this.css("position","absolute");
@@ -85,26 +72,21 @@ function myFunction() {
                 return this;
             }
 
-            document.getElementById("annonser").innerHTML += "<div class='ad'> <div class='wrap-ad'>" +  "<div class='headline'>" + annonsRubrik + "</div>" + "<br>" + "<div class='undertitle'>" + arbetsplatsNamn + "<br>" + yrkesBenamning + "</div>" + "</div>" + "<br>" + "<input type='image' src='plus.png' id=annonsimage>" + "<div id='annonsid' style='display:none'>" + annonsId + "</div>" + "</div>";
-
-
-            // + "<br>" + "<p class='job-adress'>" + jobAddress +' '+ postort + "</p>"
-
             $("input#annonsimage").click(function() {
                 var annonsid = $(this).next().html();
                 $(".helannons").show();
+                console.log('show is working');
                 $(".helannons").center();
                 console.log(annonsid);
 
               var url1 = "http://api.arbetsformedlingen.se/af/v0/platsannonser/" + annonsid;
               console.log(url);
 
-              //API call
+              //GET THE FULL AD
               var xhr = new XMLHttpRequest 
               xhr.open("GET", url1, false );
               xhr.send();
 
-              //Change from text to JSON format
               var annonsData = JSON.parse(xhr.responseText);
               console.log(annonsData);
 
@@ -113,21 +95,60 @@ function myFunction() {
               var postort = annonsData.platsannons.arbetsplats.postort;
                 console.log(jobAddress);
                 console.log(postort);
-              /*$('button.address-placeholder').click(function() {
-                  console.log("button clicked");
-                  //$(this).hide();
-              });*/
-               document.getElementById("annonser").innerHTML += "<div class='ad'>"+ "<button>"+jobAddress+"</button><p class='job-address'>"+jobAddress+"</p><br>"+ annonsRubrik + "<br>" + arbetsplatsNamn + "<br>" + yrkesBenamning + "<br>" + "<div id='annonsid' style='display:none'>" + annonsId + "</div>" + "</div>" + "<br>";
 
-              //Show all elements in 
               document.getElementById("annonsrubrik").innerHTML = annonsData.platsannons.annons.annonsrubrik;
               document.getElementById("yrke").innerHTML = annonsData.platsannons.annons.yrkesbenamning;
               document.getElementById("annonstext").innerHTML = annonsData.platsannons.annons.annonstext;
               document.getElementById("foretag").innerHTML = annonsData.platsannons.arbetsplats.arbetsplatsnamn;
-              //Hanne newcode
-              $("#sok-knapp-button").attr('href', annonsData.platsannons.annons.platsannonsUrl);
-          
-              var apiKey = 'c905773b36b7410e941b1220cf5ed883';
+            
+            function cutString(id){    
+               var text = annonsData.platsannons.annons.annonstext;         
+               var charsToCutTo = 600;
+                  if(text.length>charsToCutTo){
+                      var strShort = "";
+                      for(i = 0; i < charsToCutTo; i++){
+                          strShort += text[i];
+                      }
+                      document.getElementById("annonstext").innerHTML = strShort + "...";
+                  }            
+              };
+
+              cutString('stuff'); 
+
+
+            //Hanne newcode
+            $("#sok-knapp-button").attr('href', annonsData.platsannons.annons.platsannonsUrl);
+            
+
+            $("#minus").click(function() { 
+                $(".helannons").hide(); 
+            });
+})
+
+          }
+        }
+      }
+    }
+  }
+}
+
+/*
+$(".helannons").hide();
+$("input#annonsimage").click(function() {
+    var annonsid = $(this).next().html();
+    $(".helannons").slideToggle();
+    });
+
+
+*/
+//Hanne newcode - change to button so that the entire ad doesn't go away
+//$(".helannons").click(function() {
+//$(".helannons").hide();
+//  })
+
+
+
+              /*var apiKey = 'c905773b36b7410e941b1220cf5ed883';
               var userAddress;
               var distanceInput;
               var jobAddressForDistance;
@@ -161,35 +182,11 @@ function myFunction() {
     console.log(distanceInput);
     $(this).parent().append("<p class='job-distance'>"+distanceInput+"</p>").css('color', 'red');
 
-  });
+  });*/
 
 
-
-
-            $("#minus").click(function() { 
-                $(".helannons").hide(); 
-            });
-             })
-          }
-        }
-      }
-    }
-  }
-}
-
-/*
-$(".helannons").hide();
-$("input#annonsimage").click(function() {
-    var annonsid = $(this).next().html();
-    $(".helannons").slideToggle();
-    });
-
-
-*/
-//Hanne newcode - change to button so that the entire ad doesn't go away
-//$(".helannons").click(function() {
-//$(".helannons").hide();
-//  })
-
-
+ /*$('button.address-placeholder').click(function() {
+                  console.log("button clicked");
+                  //$(this).hide();
+              });*/
 
